72569e7f7f98a13f9a2824d8a5cb8b36de979f30
LeoGAR55 <leonardogaribaydiaz@gmail.com>
2024-10-23 21:42:43 -0600

    Tarea 8 con node

---
+++ tarea8/app.js
@@ -0,0 +1,17 @@
+// Importamos el modulo HTTP que viene incluido en Node
+const http = require('http');
+//Definir un puerto en que el va a correr el servidor
+const port = 3006;
+
+// Crear un servidor
+const server = http.createServer((req,res)=>{
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('Garibay Diaz Leonardo  319115118');
+        
+
+
+});
+
+server.listen(port, ()=>{
+    console.log(`El servidor esta en http://localhost:${port}`);
+});
